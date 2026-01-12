a1348e00028f4589bb8dd94519b2eb6247c5c7fa
David Song <han02bit18@gmail.com>
2026-01-12 15:53:25 -0800

    set up input form component and UI for home page

--- src/app/components/InputForms.tsx
+++ src/app/components/InputForms.tsx
@@ -2,3 +2,41 @@
 
 import { useState } from "react";
 import { Card } from "./Card";
+import { PlusCircle, Save } from "lucide-react";
+
+export default function InputForms() {
+  // STATES
+  const [workout, setWorkout] = useState({
+    exercise: "",
+    weight: "",
+    reps: "",
+    sets: "",
+  });
+
+  const [macros, setMacros] = useState({
+    protein: "",
+    carbs: "",
+    fat: "",
+  });
+
+  // HANDLERS
+  const handleExerciseChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWorkout({
+      ...workout,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <Card title="Workout Logger">
+        <form>
+          <div>
+            <label></label>
+            <input></input>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+}
